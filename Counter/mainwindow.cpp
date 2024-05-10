6733ae0d2a1515eb99522e6c12a27b48c56b740c
Vlada Shudegova <vlada.shudegova@gmail.com>
2024-05-10 23:42:10 +0700

    Add objects on main window and add connection

--- Counter/mainwindow.cpp
+++ Counter/mainwindow.cpp
@@ -1,15 +1,43 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QTextCodec>
+#include <QHBoxLayout>
+//#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent):
+    QWidget(parent){
+    codec = QTextCodec::codecForName("UTF-8");
+    this->setWindowTitle(codec->toUnicode("Счетчик"));
+    label1 = new QLabel(codec->toUnicode("Счет по 1"), this);
+    label2 = new QLabel(codec->toUnicode("Счет по 5"), this);
+    edit1 = new Counter("0", this);
+    edit2 = new Counter("0", this);
+    calcbutton = new QPushButton("+1", this);
+    exitbutton = new QPushButton(codec->toUnicode("Выход"), this);
+
+    QHBoxLayout *layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
+
+    QHBoxLayout *layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
+
+    QHBoxLayout *layout3 = new QHBoxLayout();
+    layout3->addWidget(calcbutton);
+    layout3->addWidget(exitbutton);
+
+    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
+
+    QObject::connect(exitbutton, &QPushButton::clicked, this, &QWidget::close, Qt::UniqueConnection);
+    QObject::connect(edit1, &Counter::tick_signal, edit2, &Counter::add_one, Qt::UniqueConnection);
+    QObject::connect(calcbutton, &QPushButton::clicked, edit1, &Counter::add_one, Qt::UniqueConnection);
 }
+
 
 MainWindow::~MainWindow()
 {
-    delete ui;
 }
 
--- Counter/mainwindow.h
+++ Counter/mainwindow.h
@@ -12,41 +12,53 @@ QT_END_NAMESPACE
 #include <QLabel>
 #include <QPushButton>
 
+
+
+// приложение, которое считает отдельные нажатия на
+//кнопку и серии по пять нажатий
+
 class Counter: public QLineEdit
 {
     Q_OBJECT
+
 public:
     Counter(const QString &contents, QWidget *parent = 0):
-        QLineEdit(contents, parent){}  // конструктор с параметрами
+        QLineEdit(contents, parent){}  // конструктор с параметрами инициализирует базовый класс QLineEdit с этими параметрами
+    //Создает строку,
+    //содержащую текстовое содержимое в качестве дочернего элемента
+    //Позиция курсора устанавливается в конец строки, а максимальная длина текста — 32767 символов.
+
+
 signals:
     void tick_signal();
+
 public slots:
     void add_one(){
-        QString str = text();
+        QString str = text(); //свойство text() содержит текст  line edit
         int r = str.toInt();
-        if(r !=0 && r%5 == 0){
+        if(r !=0 && r % 5 == 0){    //проверка на новое нажатие и на серию из 5 нажатий
             emit tick_signal();
         }
         r++;
-        str.setNum(r);
-        setText(str);
+        str.setNum(r);  //перевод числа в строку
+        setText(str);   //устанавливает текст виджета QLineEdit равным строке str, обновляя отображаемое содержимое этого виджета.
     }
 };
 
 
-class MainWindow : public QMainWindow
+class MainWindow : public QWidget
 {
     Q_OBJECT
 
 protected:
     QTextCodec *codec;
-    QLabel *label, *label2;
+    QLabel *label1, *label2;
     Counter *edit1, *edit2;
     QPushButton *calcbutton;
     QPushButton *exitbutton;
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
