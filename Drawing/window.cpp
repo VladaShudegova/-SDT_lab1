a9e04319eb0680fd126a16afce81b4139434d8a1
Влада Шудегова <v.k.shudegova@viarune.com>
2024-05-17 01:01:35 +0700

    Create file figura.h add clases Figura, MyLine, MyRect

---
+++ Drawing/window.cpp
@@ -0,0 +1,15 @@
+#include "window.h"
+#include "ui_window.h"
+
+window::window(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::window)
+{
+    ui->setupUi(this);
+}
+
+window::~window()
+{
+    delete ui;
+}
+
