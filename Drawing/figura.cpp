747e007be34f9ac23e1feb056b8a1629925726c2
Влада Шудегова <v.k.shudegova@viarune.com>
2024-05-18 01:04:54 +0700

    Create methods draw for MyRect and MyLine

--- Drawing/figura.cpp
+++ Drawing/figura.cpp
@@ -1,6 +1,27 @@
 #include "figura.h"
+#include <math.h>
 
-Figura::Figura()
+void Figura::move(float Alpha, QPainter *Painter)       //угол поворота в радианах, указателем на объект QPainter Qt, используемый для рисования в графическом контексте
 {
+    dx=halflen*cos(Alpha);  //  горизонтальное смещение от центральной точки
+    dy=halflen*sin(Alpha);  //  вертикальное смещение от центральной точки
+    draw(Painter);      //перересовка
+}
+
+void MyLine::draw(QPainter *Painter)
+{
+    if (Painter->isActive()) {  //проверка на готовность к использованию
+    Painter -> drawLine(x+dx, y+dy, x-dx, y-dy);    //Рисует линию из ( x1, y1) к ( x2, y2).
+    }
+}
+
+void MyRect::draw(QPainter *Painter){
+    if (Painter->isActive()) {
+        Painter->drawLine(x+dx, y+dy, x+dy, y-dx);  //рисуем диагонали
+        Painter->drawLine(x+dy,y-dx,x-dx,y-dy);
+        Painter->drawLine(x-dx,y-dy,x-dy,y+dx);
+        Painter->drawLine(x-dy,y+dx,x+dx,y+dy);
+
+    }
 
 }
